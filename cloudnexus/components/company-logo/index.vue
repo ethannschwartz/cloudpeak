ea8155e2a19bfd24d87ec0a09c73dd7a0f7e9067
ethannschwartz <96845694+ethannschwartz@users.noreply.github.com>
2024-05-21 04:07:06 +0300

    Implement Auth0 authentication
    The authentication system has been overhauled and now leverages the Auth0 service. Changes include the deletion, creation, and modification of multiple files including auth.ts, login/index.vue, package.json, and others. The changes facilitate user login, session management, and redirection in cases of unauthenticated access.

--- cloudnexus/components/company-logo/index.vue
+++ cloudnexus/components/company-logo/index.vue
@@ -1,10 +1,10 @@
 <template>
-  <h1 class="flex items-center gap-1.5 font-bold">
+  <h5 class="flex items-center gap-1.5 font-bold">
     <span class="text-white p-1.5 rounded-full bg-black">
       <i class="fi fi-rr-cloud-showers flex items-center text-sm"></i>
     </span>
     <span>{{ useState('companyName').value }}</span>
-  </h1>
+  </h5>
 </template>
 
 <script setup lang="ts">
